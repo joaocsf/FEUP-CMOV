6aada4195d201a8daf61dc207d5b56f1e579c1b8
joaocsf <noobcsf@hotmail.com>
2018-10-15 13:55:34 +0100

    Added Backend Docker

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+require('./routes')(app)
+/*
+* Clear Database
+  sequelize.sync({force: true})
+  Careful!!
+*/
+
+sequelize.sync()
+  .then(() => {
+    app.listen(process.env.PORT || config.port)
+    console.log(`Server started on port ${config.port}`)
+  })
+
+module.exports = app
