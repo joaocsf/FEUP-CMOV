366614c72dda9dc1a65fdfc6b41d1e6bfab00b5c
mferreira96 <pount@sapo.pt>
2018-10-15 14:53:11 +0100

    created project structure

---
+++ main-mobile-app/app/src/main/java/feup/cmpv/feup/casadamusica/view/UserRegister.java
@@ -0,0 +1,17 @@
+package feup.cmpv.feup.casadamusica.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import feup.cmpv.feup.casadamusica.R;
+
+public class UserRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_register);
+    }
+
+
+}
