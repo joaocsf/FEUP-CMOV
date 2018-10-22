a2d0a253128f601e0f300bf7774d960bdb2ad311
joaocsf <noobcsf@hotmail.com>
2018-10-22 14:58:46 +0100

    Updated Costumer Service

--- main-mobile-app/app/src/main/java/feup/cmpv/feup/casadamusica/services/CostumerServices.java
+++ main-mobile-app/app/src/main/java/feup/cmpv/feup/casadamusica/services/CostumerServices.java
@@ -4,19 +4,31 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import com.android.volley.Response;
 
+import feup.cmpv.feup.casadamusica.structures.Card;
 import feup.cmpv.feup.casadamusica.structures.Costumer;
 
 public class CostumerServices {
 
-    public static void Register(Costumer costumer, Response.Listener<JSONObject> success, Response.ErrorListener fail){
+    public static void Register(Costumer costumer, Card card, Response.Listener<JSONObject> success, Response.ErrorListener fail){
         try {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("username", costumer.getUsername());
-            jsonObject.put("name", costumer.getName());
-            jsonObject.put("password", costumer.getPassword());
-            jsonObject.put("nif", costumer.getNif());
+            JSONObject user = new JSONObject();
 
-            Api.Post("/costumer", jsonObject, success, fail);
+            user.put("username", costumer.getUsername());
+            user.put("name", costumer.getName());
+            user.put("password", costumer.getPassword());
+            user.put("nif", costumer.getNif());
+            user.put("publicKey", costumer.getPublicKey());
+
+            JSONObject cd = new JSONObject();
+            cd.put("number", card.getNumber());
+            cd.put("validity", card.getValidity());
+            cd.put("type", card.getType());
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("user",user);
+            jsonObject.put("card",card);
+
+            Api.Post("/costumer/create", jsonObject, success, fail);
         } catch (JSONException e) {
             e.printStackTrace();
         }
