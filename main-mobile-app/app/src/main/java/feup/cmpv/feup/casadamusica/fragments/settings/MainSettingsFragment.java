aa93c717b5fc67f36b5255320004c9e93b6e04b8
mferreira96 <pount@sapo.pt>
2018-11-12 08:45:54 +0000

    added qrcode reader

--- main-mobile-app/app/src/main/java/feup/cmpv/feup/casadamusica/fragments/settings/MainSettingsFragment.java
+++ main-mobile-app/app/src/main/java/feup/cmpv/feup/casadamusica/fragments/settings/MainSettingsFragment.java
@@ -8,6 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import feup.cmpv.feup.casadamusica.R;
 import feup.cmpv.feup.casadamusica.view.NFC.NFCSendActivity;
@@ -15,8 +19,7 @@ import feup.cmpv.feup.casadamusica.view.NFC.NFCSendActivity;
 public class MainSettingsFragment extends Fragment {
 
     public static Fragment getInstance() {
-        Fragment fragment = new MainSettingsFragment();
-        return fragment;
+        return new MainSettingsFragment();
     }
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -34,5 +37,33 @@ public class MainSettingsFragment extends Fragment {
             intent.putExtra("type", "order");
             startActivity(intent);
         });
+
+        Button btn_read_qr = view.findViewById(R.id.settings_read_qr);
+
+        btn_read_qr.setOnClickListener((bt) -> {
+            IntentIntegrator intentIntegrator =  IntentIntegrator.forSupportFragment(this);
+            intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+            intentIntegrator.setPrompt("Read QR_code");
+            intentIntegrator.setCameraId(0);
+            intentIntegrator.setBeepEnabled(false);
+            intentIntegrator.setOrientationLocked(false);
+            intentIntegrator.initiateScan();
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        System.out.println(requestCode + " " + resultCode + " " + data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this.getContext(), "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                System.out.println(result.getContents());
+                Toast.makeText(this.getContext(), "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
